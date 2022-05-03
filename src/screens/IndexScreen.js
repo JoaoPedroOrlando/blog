01e109f4a98e09f5103697863f26e28c14848923
joao.orlando <joao.orlando@maxiconsystems.com.br>
2022-05-03 08:24:28 -0300

    Creating context

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import {View,Text,StyleSheet} from 'react-native';
+import React, {useContext} from 'react';
+import {View,Text,StyleSheet,FlatList,Button} from 'react-native';
+import {Context} from '../context/BlogContext';
 
 const IndexScreen = () => {
+    const {state,addBlogPost} = useContext(Context);
+
     return (
         <>
             <Text>Index Screen</Text>
+            <Button
+                title="Add post"
+                onPress={()=>{
+                    addBlogPost()
+                }}
+            />
+            <FlatList
+                data ={state}
+                keyExtractor = {post => post.title}
+                renderItem={ ({item})=>{
+                    return(<View>
+                        <Text>{item.title}</Text>
+                    </View>);
+                }}
+            />
         </>
     );
 }
